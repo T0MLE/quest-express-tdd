56b4756769d75b9b085f61bc9c728d8930743688
T0MLE <lelaurain.tom@gmail.com>
2023-07-03 12:35:46 +0200

    g tout fait

--- app.js
+++ app.js
@@ -1,9 +1,71 @@
 // app.js
 const express = require('express');
 const bodyParser = require('body-parser');
+require('dotenv').config();
+const db = require("./connection");
+
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+const getAll = (req, res) => {
+    res.status(200).json({"message" : "Hello World!"})
+}
+
+const insert = (bookmark) => {
+    const { url, title } = bookmark;
+    return db.execute(
+      `insert into bookmark (url, title) values (?, ?)`,
+      [url, title]
+    );
+  };
+
+  const post = async (req, res) => {
+    const { url, title } = req.body;
+    if (!url || !title) {
+      res.status(422).send({error: "required field(s) missing"});
+      return;
+    }
+    try {
+        const result = await insert({ url, title });
+        res
+          .status(201)
+          .json({ id: result.insertId, url, title });
+      } catch (err) {
+        console.error(err);
+        res.status(500).send({
+          error: err.message,
+        });
+      }
+    };
+
+
+    const findById = (id) => {
+        return db
+          .query(
+            "SELECT * FROM bookmark WHERE id = ? ",
+            [id]
+          )
+          .then(([data]) => {
+            return data;
+          });
+      };
+
+    const getById = (req, res) => {
+        const { id } = req.params;
+        findById(id)
+          .then(([bookmark]) => {
+            !bookmark
+              ? res.status(404).json({ error: 'Bookmark not found' })
+              : res.status(200).json(bookmark);
+          })
+          .catch((err) => console.error(err));
+      };
+
+
+app.get("/", getAll )
+app.get("/bookmarks/:id", getById)
+app.post("/bookmarks", post)
 
 module.exports = app;
--- connection.js
+++ connection.js
@@ -1,14 +1,14 @@
 // connection.js
-const mysql = require('mysql');
+const mysql = require('mysql2/promise');
 
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
 });
 
-connection.connect(function(err) {
+connection.getConnection(function(err) {
   if (err) {
     console.error('error connecting: ' + err.stack);
     return;
